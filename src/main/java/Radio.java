2d5a03e1a00d837d1081be27964c5e85b87ea320
Igor Mandrik <creation@86@mail.ru>
2022-01-24 21:43:16 +0800

    Edited the initial code in Radio.java

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -2,27 +2,32 @@ public class Radio {
 
     // Создание полей
 
+    private int minStation = 0;
+    private int maxStation = 9;
     private int currentStation;
+
+    private int minVolume = 0;
+    private int maxVolume = 10;
     private int currentVolume;
 
-    // Работа с радиостанциями
+    //Работа с радиостанциями
 
     public int getCurrentStation() {
         return currentStation;
     }
 
     public void setCurrentStation(int currentStation) {
-        if (currentStation < 0) {
+        if (currentStation < minStation) {
             return;
         }
-        if (currentStation > 9) {
+        if (currentStation > maxStation) {
             return;
         }
         this.currentStation = currentStation;
     }
 
     public void nextStation() {
-        if (currentStation < 9) {
+        if (currentStation < maxStation) {
             currentStation = currentStation + 1;
         } else {
             currentStation = 0;
@@ -30,14 +35,15 @@ public class Radio {
     }
 
     public void prevStation() {
-        if (currentStation > 0) {
+        if (currentStation > minStation) {
             currentStation = currentStation - 1;
         } else {
             currentStation = 9;
         }
     }
 
-    //Работа с уровнем громкости
+    // Работа с уровнем громкости
+
 
     public int getCurrentVolume() {
         return currentVolume;
@@ -53,15 +59,17 @@ public class Radio {
         this.currentVolume = currentVolume;
     }
 
+    // Работа с уровнем громкости
+
     public void increaseVolume() {
-        if (currentVolume < 10) {
+        if (currentVolume < maxVolume) {
             currentVolume = currentVolume + 1;
         }
 
     }
 
     public void reduceVolume() {
-        if (currentVolume > 0) {
+        if (currentVolume > minVolume) {
             currentVolume = currentVolume - 1;
         }
     }
